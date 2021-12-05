6bcde5ad3cd95ddb7a986e4ae99767a86a80a333
Elijah Busick <elibusick11@gmail.com>
2021-12-05 10:28:36 -0500

    semantics and added a a for loop and created rows

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+// Var for container, time, and current hour
 var container = $(".container");
 var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var currentHour = moment().hour() - 9;
+// function to load dates and columns
+function eventLoad(){
+    for (i = 0; i < 9; i++) {
+        var row = $("<div>");
+            row.attr("class", "row");
+            container.append(row);
+        var label = $("<label>");
+            label.attr("class", "col-2 col-sm-1 time-block hour");
+            label.text(time[i]);
+            row.append(label);
+    }
+}
+
+window.onload = eventLoad();
